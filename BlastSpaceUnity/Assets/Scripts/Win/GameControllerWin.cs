0afebd8c4970ddc36e6bcbb53353e515543148dc
Gavin Schmidt <ga775440@ucf.edu>
2025-01-16 15:08:32 -0500

    High Score and Second Build

--- BlastSpaceUnity/Assets/Scripts/Win/GameControllerWin.cs
+++ BlastSpaceUnity/Assets/Scripts/Win/GameControllerWin.cs
@@ -6,10 +6,20 @@ using TMPro;
 public class GameControllerWin : MonoBehaviour
 {
     public TMP_Text scoreDisplay;
+
+    public TMP_Text highScoreDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
         int score = PlayerPrefs.GetInt("Score");
         scoreDisplay.text = score.ToString();
+
+        if (PlayerPrefs.GetInt("Score", 0) > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+
+        highScoreDisplay.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 }
